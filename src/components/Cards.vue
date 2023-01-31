218cbb69952e18c970eb558fddca17de9c5268a1
DESKTOP-CKRQI90\CON122 <tonyliang71@gmail.com>
2023-01-31 22:55:46 +0900

    fix card's hard code

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,18 +1,47 @@
 <template>
     <div class="flex">
-    <StatisticCard title="Totla Revenue To Date" statistic="11.8m" :percent-change=2.3 percent-explainer="than last year" :rising-status=1></StatisticCard>
-    <StatisticCard title="Current Month" statistic="5.5m" :percent-change=2.1 percent-explainer="than last month" :rising-status=1></StatisticCard>
-    <StatisticCard title="Totla Revenue To Date" statistic="1554/1269" :percent-change=1.8 percent-explainer="than last year" :rising-status=0></StatisticCard>
-    <StatisticCard title="Totla Revenue To Date" statistic="11.75" :percent-change=1.3 percent-explainer="than last year" :rising-status=1></StatisticCard>
+    <StatisticCard :carddata="carddata1"></StatisticCard>
+    <StatisticCard :carddata="carddata2"></StatisticCard>
+    <StatisticCard :carddata="carddata3"></StatisticCard>
+    <StatisticCard :carddata="carddata4"></StatisticCard>
     </div>
 </template>
 
 <script lang="ts">
 import StatisticCard from './StatisticCard.vue'
 export default {
-    data: () => ({
-
-    }),
+    data() {
+        return {
+            carddata1: {
+                title: "Totla Revenue To Date" as string,
+                statistic: "11.8m" as string,
+                percentChange: 2.3 as Number,
+                percentExplainer: "than last year" as string,
+                risingStatus: 1 as Number,
+            },
+            carddata2: {
+                title: "Current Month" as string,
+                statistic: "5.5m" as string,
+                percentChange: 2.1 as Number,
+                percentExplainer: "than last month" as string,
+                risingStatus: 1 as Number,
+            },
+            carddata3: {
+                title: "New/Lost Locations" as string,
+                statistic: "1554/1269" as string,
+                percentChange: 1.8 as Number,
+                percentExplainer: "than last year" as string,
+                risingStatus: 0 as Number,
+            },
+            carddata4: {
+                title: "Average SKU per Location" as string,
+                statistic: "11.75" as string,
+                percentChange: 1.3 as Number,
+                percentExplainer: "than last year" as string,
+                risingStatus: 1 as Number,
+            },
+        }
+    },
     props: {
 
     },
--- src/components/StatisticCard.vue
+++ src/components/StatisticCard.vue
@@ -1,31 +1,21 @@
 <template>
   <div class="w-1/4 rounded overflow-hidden shadow-lg hover:bg-gray-100 hover:cursor-pointer float-left ml-5">
     <div class="px-3 py-1">
-      <p class=" text-sm mb-2">{{ title }}</p>
+      <p class=" text-sm mb-2">{{ carddata.title }}</p>
       <p class="font-bold text-gray-700 text-xl">
-        {{ statistic }}
+        {{ carddata.statistic }}
       </p>
     </div>
     <div class="px-3 py-2">
-      <div v-if="risingStatus=='1'" class="text-green-500 font-bold"> 
-        <span class="float-left">{{ percentChange }}%</span>
+      <div v-if="carddata.risingStatus=='1'" class="text-green-500 font-bold"> 
+        <span class="float-left">{{ carddata.percentChange }}%</span>
         <span class="float-left ml-4">↑</span>
       </div>
-      <div v-if="risingStatus=='0'" class="text-red-500 font-bold"> 
-        <span class="float-left">{{ percentChange }}%</span>
+      <div v-if="carddata.risingStatus=='0'" class="text-red-500 font-bold"> 
+        <span class="float-left">{{ carddata.percentChange }}%</span>
         <span class="float-left ml-4">↓</span>
       </div>
-      <!-- <span v-if="this.props.risingStatus=='1'" class="ml-4 float-left">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
-        </svg>
-      </span>
-      <span v-else-if="this.props.risingStatus=='0'" class="ml-4">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
-        </svg>
-      </span> -->
-      <span class="ml-4">{{ percentExplainer }}</span>
+      <span class="ml-4">{{ carddata.percentExplainer }}</span>
     </div>
   </div>
 </template>
@@ -37,11 +27,12 @@ export default {
   }),
   name: "StatisticCard",
   props: {
-    title: String,
-    statistic: String,
-    percentChange: Number, 
-    percentExplainer: String,
-    risingStatus: Number,
+    carddata:{
+      type: Object,
+      default() {
+        return {};
+      },
+    },
   },
   components: {
 
